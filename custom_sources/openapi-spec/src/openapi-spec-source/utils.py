4e162401b2d728feea5115556ee99c38278473cf
Guido Schmutz <guido.schmutz@trivadis.com>
2024-06-28 12:39:40 +0200

    Update utils.py

--- custom_sources/openapi-spec/src/openapi-spec-source/utils.py
+++ custom_sources/openapi-spec/src/openapi-spec-source/utils.py
@@ -22,8 +22,8 @@ def get_arch_repo_json(path: str):
     return arch_repo_json
 
 def get_api_spec(url: str, system: str, system_component: str): 
-    url = url.replace("<system>", system)
-    url = url.replace("<system_component>", system_component)
+    url = url.replace("{system}", system)
+    url = url.replace("{system_component}", system_component)
     url_parsed = parse.urlparse(url)
     if url_parsed.scheme not in ("http", "https"):  # A local file
         raise Exception(f"Must be a valid url: {url}, error:{e}")
@@ -43,7 +43,7 @@ def download_model(model_path: str, model_file: str):
     with open(model_file, 'w') as file:
         file.write(arch_repo_model_str) 
                        
-def download_api_specs(model_path: str, api_spec_url: str, output_folder: str, p_system: str = None, p_system_component: str = None):
+def download_api_specs(model_path: str, api_spec_url: str, output_file: str, p_system: str = None, p_system_component: str = None):
     arch_repo_model_json = get_arch_repo_json(model_path)
     for system in arch_repo_model_json["systems"]:
         system_name = system["name"]
@@ -59,8 +59,8 @@ def download_api_specs(model_path: str, api_spec_url: str, output_folder: str, p
                     api_spec_json = get_api_spec(api_spec_url, system_name, system_component_name)
                     api_spec_json_str = json.dumps(api_spec_json, indent=4)                      
                     
-                    api_output_folder = output_folder.replace("<system>", system_name).replace("<system_component>", system_component_name)
-                    with open(api_output_folder + "/raw", 'w') as file:
+                    api_output_file = output_file.replace("{system}", system_name).replace("{system_component}", system_component_name)
+                    with open(api_output_file, 'w') as file:
                         file.write(api_spec_json_str)
                         
 def main():
@@ -70,7 +70,7 @@ def main():
     parser.add_argument('-mp', '--model-path', type=str, action='store', help='Specify the model path, if command is `download_model` or `download_api_specs`', required=True)
     parser.add_argument('-mf', '--model-file', type=str, action='store', help='Specify the model file, if command is `download_model`', required=False)
     parser.add_argument('-apiurl', '--api-spec-url', type=str, action='store', help='Specify the api-spec url, if command is `download_api_specs`', required=False)
-    parser.add_argument('-apiout', '--api-spec-output-folder', type=str, action='store', help='Specify the api-spec output folder, if command is `download_api_specs`', required=False)
+    parser.add_argument('-apiout', '--api-spec-output-file', type=str, action='store', help='Specify the api-spec output file, if command is `download_api_specs`', required=False)
     parser.add_argument('-s', '--system', action='store', type=str, help='Specify the system to use, if command is `download_api_spec`', required=False)
     parser.add_argument('-sc', '--system-component', type=str, action='store', help='Specify the model path, if command is `download_api_spec`', required=False)
     parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose mode', required=False)
@@ -81,7 +81,7 @@ def main():
         download_model(model_path=args.model_path, model_file=args.model_file)
     elif (args.command == "download_api_spec"):
                     
-        download_api_specs(model_path=args.model_path, api_spec_url=args.api_spec_url, output_folder=args.api_spec_output_folder, p_system=args.system, p_system_component=args.system_component)
+        download_api_specs(model_path=args.model_path, api_spec_url=args.api_spec_url, output_file=args.api_spec_output_file, p_system=args.system, p_system_component=args.system_component)
     
 if __name__ == '__main__':
     main()    
